e933ffdec3eab5a62c54bf7d9222650e25bdb50a
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2025-06-03 04:19:48 +0300

    add lesson 1

---
+++ 01_Working_With_Numbers/01_Python_Syntax.py
@@ -0,0 +1,24 @@
+###################
+# 1- print a message
+# 2- Python is case-sensitive
+# 3- Writing Comments
+###################
+
+# 1- print a message
+print("Hello, Python learners!")
+
+# 2- Python is case-sensitive
+name = "Mohamed"
+Name = "Ahmed"
+
+print(name)
+print(Name)
+
+# 3- Writing Comments
+# This is a single-line comment
+"""
+    This is a multiple line comment
+    Line 1
+    Line 2
+    Line 3
+"""
