025f1023944b0dbe503a9b8c4923cfa895bf2c5a
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2025-06-06 02:40:27 +0300

    update working with numbers

---
+++ 01_Numbers_And_Text/08_string_formatting.py
@@ -0,0 +1,19 @@
+#######################
+    # String Formatting
+#######################
+
+name = "Mohamed"
+age = "25"
+job = "Teaching Assistant"
+text = "My name is " + name + ". I'm " + age + " years old. " + "I work as a " + job + " at Misr University"
+text2 = f"My name is {name}. I'm {age} years old. I work as a {job} at Misr University"
+print(text)
+print(text2)
+
+num1 = 10
+num2 = 5
+result = f"Sum = {num1 + num2}"
+print(result)
+
+price = 50.74123123123
+print(f"product price = {price:.1f}")
