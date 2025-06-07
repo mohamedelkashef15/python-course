a199118ac1dc2511dce19c487be83027402cc7d3
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2025-06-08 00:51:15 +0300

    update working with numbers

---
+++ 01_Numbers_And_Text/13_comparsion_operators.py
@@ -0,0 +1,25 @@
+#################
+# Comparsion Operators
+# Equal ==
+# Not Equal !=
+#
+#################
+
+print(5 == 5)
+print(10 == 3)
+
+print(10 != 5)
+print(5 != 5)
+
+print(5 > 3)
+print(5 < 3)
+
+print(8 >= 8)
+print(8 >= 3)
+
+print(8 <= 3)
+print(8 <= 8)
+
+# ? search for ascii code
+print("hello" == "Hello")
+print(5 == "5")
