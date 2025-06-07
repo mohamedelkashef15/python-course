81d1f19b38cb18659b96c623950f1c7ec03094af
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2025-06-08 00:40:01 +0300

    update working with numbers

---
+++ 01_Numbers_And_Text/11_arithmetic_operators.py
@@ -0,0 +1,23 @@
+########################
+# Operators
+#  Arithmetic Operators
+#  +, -, *, /, //, %, **
+# ? what is modulus and explain it
+########################
+add = 5 + 3
+subtract = 10 - 4
+multiply = 5 * 3
+division = 10 / 2
+floor_division = 12 // 5
+modulus = 10 % 3
+exponentiation = 2**3
+# 2 * 2 * 2
+print(add)
+print(subtract)
+print(multiply)
+print(division)
+print(floor_division)
+print(modulus)
+print(exponentiation)
+
+
---
+++ 01_Numbers_And_Text/12_assignment_operators.py
@@ -0,0 +1,42 @@
+######################
+# Assignment Operators
+#  =
+#  +=
+#  -=
+#  *=
+#  **=
+#  /=
+#  %=
+######################
+name = "Mohamed"
+print(name)
+
+# new Name = old name + " Elkashef"
+name = name + " Elkashef"
+print(name)
+
+x = 3
+print(x)
+
+# new x = old x + 2
+x = x + 2 # 5
+x += 2
+print(x) # 7
+
+y = 10
+# y = y - 4
+y -= 4
+
+print(y)
+
+z = 5
+z *= 3
+# z = z * 3
+print(z)
+
+a = 5
+# a = a ** 3
+# 5 * 5 * 5
+a **= 3
+
+print(a)
