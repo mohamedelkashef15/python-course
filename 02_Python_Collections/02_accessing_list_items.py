1294d6b0e741bbcdf8f9a2829610d25a675a035c
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2025-07-06 16:08:42 +0300

    update Python Collections

--- 02_Python_Collections/02_accessing_list_items.py
+++ 02_Python_Collections/02_accessing_list_items.py
@@ -1,47 +1,40 @@
 #########################
-#   Accessing List items
-#   Changing List items
+#   Lists
+#     - Accessing List items
+#     - Changing List items
 #########################
 
 fruits = ["Orange", "Mango", "Banana", "Apple", "Avocado", "Coconut", "Guava"]
 
-# length
-print(len(fruits)) # 7
+# Accessing List items
+print(fruits)
+print(fruits[0])
+print(fruits[2])
+print(fruits[-1])
 
-# access first item
-print(fruits[0]) # Orange
-
-# Access third item
-print(fruits[2]) # Banana
-
-# access last item in the list
-print(fruits[-1]) # Guava
-
-# Slicing lists [End is not included]
+# Slicing list [End is not included]
 print(fruits[2:])
 print(fruits[:2])
-print(fruits[2:5]) # ["Banana", "Apple", "Avocado"]
+print(fruits[2:5])
 
-# Access nested lists
+# Acess nested list
 nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
+print(nested_list)
 print(nested_list[0])
-print(nested_list[0][2]) # 3
-print(nested_list[2][0]) # 7
+print(nested_list[0][2])
+print(nested_list[2][1])
 
-#   Changing List items
-# Lists are mutable, meaning you can change their elements after creation.
+# Changing List items
 colors = ["red", "green", "blue"]
 
-# Change single element
+print(colors)
 colors[1] = "yellow"
-print(colors)  # ["red", "yellow", "blue"]
 
-# Change multiple elements with slicing
+print(colors)
+
 colors[0:2] = ["pink", "purple"]
-print(colors)  # Output: ["pink", "purple", "blue"]
-
-
+print(colors)
 
 
 
