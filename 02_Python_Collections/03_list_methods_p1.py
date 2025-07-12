04149906e8d2918667e3cc9649542b6ce0c10698
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2025-07-12 22:44:15 +0300

    update Python Collections

---
+++ 02_Python_Collections/03_list_methods_p1.py
@@ -0,0 +1,73 @@
+######################
+# Add and Remove List Items
+#   Adding
+#     - append()
+#     - insert()
+#     - extend()
+#     - concatenation (+)
+#     - * repetation
+#   Removing
+#     - remove() => first occurance of a value
+#     - pop() => remove last item
+#     - del => remove item or entire list
+#     - clear() => removes all elements inside list
+######################
+
+names = ["Mohamed", "Ahmed"]
+
+print(names)
+# append()
+names.append("Tarek")
+
+# names.append(["Malak", "Farida"])
+# names.append("abc")
+print(names)
+
+# insert(index, value)
+names.insert(1, "Islam")
+print(names)
+
+# extend()
+more_names = ["Malak", "Farida"]
+
+names.extend(more_names)
+# names.extend("abc")
+print(names)
+
+# concatenation (+)
+all_names = names + ["Farah", "Sarah"]
+
+print(all_names)
+
+# * repetation
+numbers = [1, 2, 3] * 3
+print(numbers)
+
+######################################
+languages = ["Python", "Java", "C++", "Ruby", "JavaScript", "Java"]
+
+print(languages)
+
+# remove() => first occurance of a value
+languages.remove("Java")
+print(languages)
+
+# pop() => remove last item
+languages.pop()
+print(languages)
+
+# del => remove item or entire list
+del languages[1]
+
+# del languages
+
+print(languages)
+
+# clear() => removes all elements inside list
+languages.clear()
+print(languages)
+
+
+
+
+
