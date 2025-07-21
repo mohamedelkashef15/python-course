d80e643b769af3d521c8468dd601775e040c7c9e
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2025-07-21 15:39:47 +0300

    update Python Collections

--- 02_Python_Collections/05_tuple_intro.py
+++ 02_Python_Collections/05_tuple_intro.py
@@ -9,34 +9,39 @@
 # 5- unchangeable, meaning that we cannot change, add or remove items after the tuple has been created.
 ########################
 
+
 # Difference between mutable vs immutable
 # mutable:
 #   - Can be changed after creation
 #   - Allow modification of contents
 #   - Examples: lists, dictionaries, sets
-my_list = [1, 2, 3]
 
+my_list = [1, 2, 3]
 print(my_list)
 
 my_list[0] = 99
 print(my_list)
+my_list.append(4)
+print(my_list)
+
 
 # Immutable
 # Cannot be changed after creation
 # Any "modification" creates a new object
 # Examples: tuples, strings, integers, floats
-my_tuple = (1, 2, 3)
-# my_tuple[0] = 99
-print(my_tuple)
 
-my_string = "python"
-# my_string[0] = "t"
-print(my_string)
+my_tuple = (1, 2, 3, "hello", 3.14, True)
 
 print(my_tuple, type(my_tuple))
+# my_tuple[0] = 99
 
-# tuple packing without parenthesess
+print(my_tuple)
+print(my_tuple[3])
+
+my_string = "python"
+# my_string[0] = "y"
+
+print(my_string)
+
 colors = "red", "green", "blue"
-
 print(colors, type(colors))
-
