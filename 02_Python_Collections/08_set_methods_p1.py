5363a8df2f1c028dfdba571b51ec2ac4ed4ba63c
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2025-07-24 15:29:11 +0300

    update Python Collections

--- 02_Python_Collections/08_set_methods_p1.py
+++ 02_Python_Collections/08_set_methods_p1.py
@@ -1,21 +1,21 @@
 ##########################
-# Set Methods
+# Set Methods part 1
 #   - add() => insert a single element
 #   - update() => insert a multiple elements at once
+#   - copy() => return a copy from set
 #   - remove() => raises KeyError if value doesn’t exist.
 #   - discard() => does nothing if value doesn’t exist.
-# ? why we use discard
 #   - pop() => remove a random element
 #   - clear() => clear entire set
 ##########################
 
 colors = {"red", "green", "blue"}
 
-# add
+# add()
 colors.add("yellow")
 print(colors)
 
-# update
+# update()
 colors.update(["purple", "black", "brown"])
 print(colors)
 
@@ -27,23 +27,37 @@ set2 = {3, 4, 5}
 set1.update(set2)
 print(set1)
 
+# copy()
+set3 = set1.copy()
+
+print(set3)
+
 # remove()
 numbers = {1, 2, 3, 4, 5}
-
+print(numbers)
 numbers.remove(3)
+
 print(numbers)
 
 # numbers.remove(10)
-# print(numbers) # Error because 10 is not exist
+# print(numbers)
 
-# discard()
-numbers.discard(10) # will not show error even if 10 is not exist
+# discard
+numbers.discard(2)
+print(numbers)
+
+numbers.discard(10)
 print(numbers)
 
 # pop()
+print(colors)
 popped = colors.pop()
+
 print(colors, popped)
 
-# clear
+# clear()
 colors.clear()
 print(colors)
+
+
+
