40d3dff4105f4e425e1ff1f12b3a057882e1bcc0
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2025-08-19 21:07:59 +0300

    update Python Collections

--- 02_Python_Collections/13_dictionaries_methods_p2.py
+++ 02_Python_Collections/13_dictionaries_methods_p2.py
@@ -1,54 +1,54 @@
 #########################
 # Dictionaries Methods Part 2
-#   - pop() => Remove and get a specific item by adding it's key
-#   - popItem() => Remove and get the last added item
-#   - setDefult() => Get a value or set it if missing
-#   - fromkeys() => Create a dictionary with default values
-# This method saves you from writing repetitive code when setting up default values!
+#   - pop()
+#   - popItem()
+#   - setDefult()
+#   - fromkeys()
 #########################
 
-# pop()
 supermarket = {
     "milk": 2,
     "eggs": 12,
     "bread": 1,
-    "Chips": 4,
+    "chips": 4
 }
 
-print(f"Before {supermarket}")
+# pop()
+print(f"before {supermarket}")
 supermarket.pop("milk")
-print(f"After {supermarket}")
+print(f"after {supermarket}")
 
-# popItem()
+# popitem()
 supermarket.popitem()
 print(supermarket)
 
-# setDefault()
-cars = {
+# setDefult()
+car = {
     "brand": "Hyundai",
     "year": "2026",
-    # "model": "Elantra",
 }
 
-default = cars.setdefault("model", "Unknown")
+car.setdefault("model", "Unknown")
 
-print(cars)
+print(car)
 
 # fromkeys()
-# Example 1
-absent_students = ["Mohamed", "Ahmed", "Mostafa", "Islam"]
-
+absent_students = ["Mohamed", "Ahmed", "Mostafa", "Gamal", "Maged"]
 attendance = dict.fromkeys(absent_students, "absent")
 
 print(attendance)
 
-# Example 2
-fruits = {
-    "apples",
-    "banana",
-    "orange",
-    "mango"
-}
 
-fruits_initial_price = dict.fromkeys(fruits, "0.00")
-print(fruits_initial_price)
+
+
+
+
+
+
+
+
+
+
+
+
+
