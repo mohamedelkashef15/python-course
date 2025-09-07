47c92bda0451706a7d3b3650afdeeddba39675e3
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2025-09-07 14:59:44 +0300

    update Control flow and loops

---
+++ 03_Control_Flow_And_Loops/05_ternary_operator.py
@@ -0,0 +1,26 @@
+#########################
+# ternary conditional operator
+
+# [return this if True] if [codition] else [return this if False]
+#########################
+
+age = 16
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
+
+print("Adult") if age >= 18 else print("Minor")
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 03_Control_Flow_And_Loops/06_for_loop.py
@@ -0,0 +1,53 @@
+##############################
+# For loop
+# allow you to execute a block of code repeatedly until a specific condition is met. They help automate repetitive tasks and make code more efficient and readable
+
+# for variable in sequence
+#   code to execute repeatedly
+##############################
+
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+# print(6)
+# print(7)
+# print(8)
+# print(9)
+# print(10)
+
+# ? Explain step in range
+for i in range(1, 11):
+    print(i)
+
+# i = 1
+# i = 2
+# i = 3
+# ....
+# i = 10
+print("#"*20)
+
+for i in range(6):
+    print(i)
+
+
+
+fruits = ["apple", "banana", "orange"]
+
+for fruit in fruits:
+  print(fruit)
+
+# fruit = apple
+# fruit = banana
+# fruit = orange
+
+name = "Mohamed"
+for s in name:
+    print(s)
+
+
+
+
+
+
