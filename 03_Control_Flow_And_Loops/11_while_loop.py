8858ff25a7416e177630c766c4fa42c36a011375
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2025-09-14 17:34:36 +0300

    update Control flow and loops

--- 03_Control_Flow_And_Loops/11_while_loop.py
+++ 03_Control_Flow_And_Loops/11_while_loop.py
@@ -1,62 +1,36 @@
-####################
+#######################
 # While Loop
-#
+
 # initialization
 # while condition:
 #   code to execute
 #   increment
+#######################
 
+# Count from 1 to 5 using for loop
+for count in range(1, 6, 2):
+    print(f"Count {count}")
 
-# ? Compare between While and for loop
-# While                              | For
-# When iterations are unknown        | When iterations are known
-# Based on Boolean condition         | Based on sequence/range
-# Manual initialization              | Automatic initialization
-# Manual increment                   | Automatic increment
-# When condition becomes false       | When sequence ends
-####################
+print("Using while loop")
 
-# count from 1 to 5
 count = 1
 while count <= 5:
-    print(f"Count: {count}")
-    count += 1
+    print(f"Count {count}")
+    count += 2
 
-# count = 1
 # Output
-# Count: 1
-# Count: 2
-# Count: 3
-# Count: 4
-# Count: 5
+# Count 1
+# Count 2
+# Count 3
+# Count 4
+# Count 5
 
+# Example
+password = ""
 
-print("Using for Loop")
-# Using for loop
-for count in range(1, 5):
-    print(f"Count: {count}")
+while password != "hello123":
+    password = input("Enter the password: ")
+    if password != "hello123":
+        print("Incorrect Password. Try again.")
 
-
-# Example: Remove vowels from a string until only consonant remain
-text = "aeioubcdfghjklmnpqrstvwxyz"
-vowels = "aeiou"
-result = ""
-
-while text[0] in vowels:
-    text = text[1:]  # Remove first character
-    print(text)
-
-print(f"Final text starts with consonant: {text}")
-
-print(result)
-
-# text = "aeioubcdfghjklmnpqrstvwxyz"
-
-# text[0] = a
-# text = "eioubcdfghjklmnpqrstvwxyz"
-
-# text[0] = e
-# text = "ioubcdfghjklmnpqrstvwxyz"
-
-# text[0] = i
-# text = "oubcdfghjklmnpqrstvwxyz"
+print("Access granted!")
