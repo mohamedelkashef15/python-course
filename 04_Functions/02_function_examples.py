5768ebb8fd4772ed803050d346685bced8f1261a
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2025-09-18 18:21:47 +0300

    update Control flow and loops

---
+++ 04_Functions/02_function_examples.py
@@ -0,0 +1,57 @@
+###########################
+# Function Examples
+###########################
+
+name1 = "Mohamed"
+print(f"Hello {name1}, Welcome to our program")
+print(f"How are you {name1}?")
+print("----")
+
+name2 = "Ahmed"
+print(f"Hello {name2}, Welcome to our program")
+print(f"How are you {name2}?")
+print("----")
+
+name3 = "Tarek"
+print(f"Hello {name3}, Welcome to our program")
+print(f"How are you {name3}?")
+print("----")
+
+name4 = "Islam"
+print(f"Hello {name4}, Welcome to our program")
+print(f"How are you {name4}?")
+print("----")
+
+name5 = "Malak"
+print(f"Hello {name5}, Welcome to our program")
+print(f"How are you {name5}?")
+print("----")
+
+print("Using function")
+
+def greeting_person(name):
+    print(f"Hello {name}, Welcome to our program")
+    print(f"How are you {name}?")
+    print("----")
+
+greeting_person("Mohamed")
+greeting_person("Ahmed")
+greeting_person("Tarek")
+greeting_person("Islam")
+greeting_person("Malak")
+
+######################
+# Function without parameters
+def greet():
+    print("Hello, Welcome to our program")
+    print("I am learning python")
+    print("Learning python with flask is fun")
+    print("---")
+
+greet()
+greet()
+greet()
+
+
+
+
