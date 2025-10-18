14192a7f9a943c0a66b3dadf73d2a36a12d07f6b
mohamedelkashef <mohamedelkashef38@gmail.com>
2025-10-18 17:59:31 +0300

    update functions

--- 04_Functions/07_global_keyword.py
+++ 04_Functions/07_global_keyword.py
@@ -2,18 +2,21 @@
 # Global Keyword
 ########################
 
-
-# Global keyword
-
 counter = 0
 
 def increment():
-    # global counter  # ✅ Tell Python we want to use the global counter
-    # print(counter)
     global counter
     counter += 1
-    print(f"Counter: {counter}")
 
-increment()  # Output: Counter: 1
-increment()  # Output: Counter: 2
-increment()  # Output: Counter: 3
+    print(f"inside counter {counter}")
+
+increment()
+increment()
+
+print(f"outside counter {counter}")
+
+# counter 0 Global
+# counter 1
+# counter 2
+
+
