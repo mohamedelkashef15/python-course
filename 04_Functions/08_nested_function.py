3c651bccccd25c2774fa0346ab9e97212f0c5ff2
mohamedelkashef <mohamedelkashef38@gmail.com>
2025-10-19 22:09:35 +0300

    update functions

--- 04_Functions/08_nested_function.py
+++ 04_Functions/08_nested_function.py
@@ -1,34 +1,14 @@
-#######################
+########################
 # Nested Function
-#######################
-# Example 1
+########################
+
 def outer_function():
     print("I'm the outer function")
-
     def inner_function():
         print("I'm the inner function")
+    inner_function()
 
-    inner_function()  # inner function
-
-
-# Usage
 outer_function()
-
-# Example 2
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
