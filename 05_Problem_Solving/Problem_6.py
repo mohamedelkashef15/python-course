d45d2d38ababb8ad5938c0cd449ce69b73ead144
mohamedelkashef <mohamedelkashef38@gmail.com>
2025-10-22 12:23:04 +0300

    update Problem Solving

---
+++ 05_Problem_Solving/Problem_6.py
@@ -0,0 +1,29 @@
+#############################
+# Problem 6
+# * Problem definition
+# Write a Python program that that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+#############################
+# 1- Ask the user to enter an integer number.
+# 2- Check whether the number is even or odd.
+# 3- Display the result on the screen.
+
+num = int(input("Enter an integer number: "))
+
+# Step 2: Check if even or odd
+if num % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 05_Problem_Solving/Problem_7.py
@@ -0,0 +1,32 @@
+#############################
+# Problem 7
+# * Problem definition
+# Write a Python program that that takes an integer as an argument and returns it as negative number
+#############################
+# 1- Ask the user to enter a number.
+# 2- Check if the number is already negative.
+# 3- If the number is positive, multiply it by -1 to make it negative.
+# 4- If it’s already negative, leave it as it is.
+# 5- Display the negative value.
+
+num = float(input("Enter a number: "))
+if num > 0:
+    num = -num
+
+print("Negative value:", num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
