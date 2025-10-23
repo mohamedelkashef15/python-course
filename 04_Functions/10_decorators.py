aa795cdbe00ccfdc4147b1bcd14da636f84db7bd
mohamedelkashef <mohamedelkashef38@gmail.com>
2025-10-23 22:34:28 +0300

    update Functions

--- 04_Functions/10_decorators.py
+++ 04_Functions/10_decorators.py
@@ -2,39 +2,37 @@
 # Function Decorators
 # Decorators are functions that modify the behavior of other functions without permanently modifying them. They're a powerful tool for adding functionality to existing code.
 # - A decorator takes a function, adds some functionality, and returns it.
-# ? why we use decorators
-# Imagine you have several functions, and you want to add the same functionality to all of them:
 #######################
 
 def my_decorator(func):
     def wrapper():
-        print("Before function modification")
+        print("Before Modification")
         func()
-        print("After function modification")
-    return  wrapper
+        print("After Modification")
+    return wrapper
 
 @my_decorator
-def say_hello():
-    print("Hello World")
+def say_greetings():
+    print("Welcome to the company")
 
-say_hello()
-print("----------")
-# Example and why we use decorators
-# * We want to add timing to our functions
+say_greetings()
+
+# Example
+
 # def good_morning(name):
-#     print("Function Started....")
+#     print("Function Started...")
 #     print(f"Good Morning {name}")
-#     print("Function Ended....")
+#     print("Function Ended...")
 #
 # def good_evening(name):
-#     print("Function Started....")
+#     print("Function Started...")
 #     print(f"Good Evening {name}")
-#     print("Function Ended....")
+#     print("Function Ended...")
 #
 # def good_night(name):
-#     print("Function Started....")
+#     print("Function Started...")
 #     print(f"Good Night {name}")
-#     print("Function Ended....")
+#     print("Function Ended...")
 #
 # good_morning("Mohamed")
 # print("----------")
@@ -42,29 +40,37 @@ print("----------")
 # print("----------")
 # good_night("Mohamed")
 
-# Using Decorator
 def my_decorator(func):
     def wrapper(name):
-        print("Function Started....")
+        print("Function Started...")
         func(name)
-        print("Function Ended....")
+        print("Function Ended...")
     return wrapper
-
 
 @my_decorator
 def good_morning(name):
-    print(f"Good Morning, {name}!")
+    print(f"Good Morning {name}")
 
 @my_decorator
 def good_evening(name):
-    print(f"Good Evening, {name}!")
+    print(f"Good Evening {name}")
 
 @my_decorator
 def good_night(name):
-    print(f"Good Night, {name}!")
+    print(f"Good Night {name}")
 
 good_morning("Mohamed")
-print("-------------")
+print("----------")
 good_evening("Mohamed")
-print("-------------")
+print("----------")
 good_night("Mohamed")
+
+
+
+
+
+
+
+
+
+
